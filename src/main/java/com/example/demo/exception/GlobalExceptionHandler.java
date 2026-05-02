9c1fe3cb3d805b87ee375ce9f23760520b2cdc54
shena <msnehamanjunatha7@gmail.com>
2026-05-02 14:51:07 +0530

    Day 6 search user by email

---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleUserNotFoundException(
+            UserNotFoundException ex) {
+
+        return Map.of(
+                "message", ex.getMessage()
+        );
+    }
+}
